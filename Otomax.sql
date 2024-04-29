3edc4a28405e4724c96003f68788b2fd999fde5c
gdslhack <34924797+gdslhack@users.noreply.github.com>
2024-04-29 19:54:35 +0700

    Add files via upload

---
+++ Otomax.sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Waktu pembuatan: 29 Apr 2024 pada 14.51
+-- Versi server: 8.0.35-0ubuntu0.22.04.1
+-- Versi PHP: 8.1.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Otomax`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `history_chat`
+--
+
+CREATE TABLE `history_chat` (
+  `id` int NOT NULL,
+  `message` text COLLATE utf8mb4_general_ci NOT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(50) COLLATE utf8mb4_general_ci DEFAULT 'unsent'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Trigger `history_chat`
+--
+DELIMITER $$
+CREATE TRIGGER `new_message_trigger` AFTER INSERT ON `history_chat` FOR EACH ROW BEGIN
+    -- Set status = 1 pada tabel message_status
+    UPDATE message_status SET status = 1 WHERE id = 1;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `member`
+--
+
+CREATE TABLE `member` (
+  `memberid` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `pin` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `message_status`
+--
+
+CREATE TABLE `message_status` (
+  `id` int NOT NULL,
+  `message_id` int DEFAULT NULL,
+  `status` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int NOT NULL,
+  `refID` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `product` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `dest` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `memberID` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `response` text COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `history_chat`
+--
+ALTER TABLE `history_chat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`memberid`);
+
+--
+-- Indeks untuk tabel `message_status`
+--
+ALTER TABLE `message_status`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `message_id` (`message_id`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `history_chat`
+--
+ALTER TABLE `history_chat`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `message_status`
+--
+ALTER TABLE `message_status`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `message_status`
+--
+ALTER TABLE `message_status`
+  ADD CONSTRAINT `message_status_ibfk_1` FOREIGN KEY (`message_id`) REFERENCES `history_chat` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
