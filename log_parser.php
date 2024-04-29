840da39768e4cdeddfe7e369a447e805fccdfc73
gdslhack <34924797+gdslhack@users.noreply.github.com>
2024-04-29 19:41:23 +0700

    Update log_parser.php

--- log_parser.php
+++ log_parser.php
@@ -3,7 +3,7 @@
 include 'database.php';
 
 // Parse pesan log
-$log = '2024-04-29 04:35:28 103.153.188.91 - - [29/Apr/2024:04:35:28 +0700] "GET /OtomaX/report.php?t=43527&refid=reffid%3Dapisik543576&status=20&price=0&message=R%2311533%20-%20Dedy%20SIK%20-%20%20CPLN.45102735896%20SUKSES.%20SN%2FRef%3A%20idpel%3D546402626546%2Fnometer%3D45102735896%2Fnama%3DM%20FUDOLI%20AJ%2Ftarifdaya%3DR1M%2F900.%20Saldo%20-20.735%20-%20hrg0%3D%20Sisa%20Saldo-20.735%20%4029%2F04%2004%3A06%3A03%20%20*TRX%20AMAN* 1.1" 200 246 "-" "-"';
+$log = '2024-04-29 04:35:28 ip-otomax - - [29/Apr/2024:04:35:28 +0700] "GET /OtomaX/report.php?t=
 
 preg_match('/^(\S+)\s(\S+)\s(\S+)\s(\S+)\s\[(.*?)\]\s\"(.*?)\"\s(\S+)\s(\S+)$/', $log, $matches);
 
